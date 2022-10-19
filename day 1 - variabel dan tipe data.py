d88dc8c41eb9d51522f96b18fd09f410914ceaa7
ikhsan <ikhsanyaaa@gmail.com>
2022-10-19 18:51:49 +0800

    first commit

---
+++ day 1 - variabel dan tipe data.py
@@ -0,0 +1,16 @@
+buah = 'apel' #string
+harga = 4000 #integer
+diskon = False #boolean
+berat = 1.3 #float
+
+nama = "ikhsan"
+Nama = "ikhsan"
+
+pisang = 20
+type(pisang)
+
+print(buah)
+print("harga apel adalah : ",harga)
+print("apakah sedang diskon? :", diskon)
+print("ibu beli berapa", berat, "kg")
+print(type(pisang))
