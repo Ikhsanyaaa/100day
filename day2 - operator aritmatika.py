4bec2c5ced793cd874913ccc76cf2e5214312b7b
ikhsan <ikhsanyaaa@gmail.com>
2022-10-20 19:18:24 +0800

    sudah

---
+++ day2 - operator aritmatika.py
@@ -0,0 +1,31 @@
+#operator aritmatika
+print(1+1)
+a = 4
+b = 2
+#penjumlahan
+hasil = (a + b)
+print("hasil dari ",a,"+",b,"adalah = ", hasil)
+
+#pengurangan
+hasil = (a - b)
+print("hasil dari ",a,"-",b,"adalah = ", hasil)
+
+#perkalian
+hasil = (a * b)
+print("hasil dari ",a,"*",b,"adalah = ", hasil)
+
+#pembagian
+hasil = (a / b)
+print("hasil dari ",a,"/",b,"adalah = ", hasil)
+
+#pangkat
+hasil = (a ** b)
+print("hasil dari ",a,"**",b,"adalah = ", hasil)
+
+#pembagian bulat
+hasil = (a // b)
+print("hasil dari ",a,"//",b,"adalah = ", hasil)
+
+#modulus / sisabagi
+hasil = (a % b)
+print("hasil dari ",a,"%",b,"adalah = ", hasil)
