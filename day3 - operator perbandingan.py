53b03eefa9e22a03c04931ef50ef7501ca816e93
ikhsan <ikhsanyaaa@gmail.com>
2022-10-21 20:00:46 +0800

    operator perbandingan

---
+++ day3 - operator perbandingan.py
@@ -0,0 +1,38 @@
+#operator perbandingan
+
+a = 5
+#sama dengan
+print("sama dengan")
+print("a sama dengan 5 : ", a == 5)
+print("a sama dengan 4 : ",a == 4)
+#ketika syarat yang diberikan bernilai sama maka output akan menunjukkan nilai True
+
+#sama seperti operator perbandingan lain, ketika memenuhi syarat yang diberikan 
+#maka akan bernilai True dan bernilai False jika tidak memenuhi
+
+#lebih kecil
+print("lebih kecil")
+print("a lebih kecil dari 6 : ",a < 6)
+print("a lebih kecil dari 4 : ",a > 4)
+
+#lebih kecil atau sama dengan
+print("lebih kecil atau sama dengan")
+print("a lebih kecil atau sama dengan 5 : ",a <= 5)
+print("a lebih kecil atau sama dengan 4: ",a <= 4)
+print("a lebih kecil atau sama dengan 6 : ",a <= 6)
+
+#lebih besar
+print("lebih besar")
+print("a lebih besar dari 4 : ",a > 4)
+print("a lebih besar dari 6 : ",a > 6) 
+
+#lebih besar atau sama dengan
+print("lebih besar atau sama dengan")
+print("a lebih besar atau sama dengan 5 : ",a >= 5)
+print("a lebih besar atau sama dengan 4 : ",a >= 4)
+print("a lebih besar atau sama dengan 6 : ",a >= 6)
+
+#tidak sama dengan
+print("tidak sama dengan")
+print("a tidak sama dengan 4 : ",a != 4)
+print("a tidak sama dengan 5 : ",a != 5)
