57371fd5327293c21a0b68a2c3d32c2b47bc42e7
Ikhsan <101315811+Ikhsanyaaa@users.noreply.github.com>
2022-10-22 23:18:31 +0800

    Add files via upload

---
+++ day4 - operator logika.py
@@ -0,0 +1,22 @@
+#operator logika
+
+#operator logika and akan bernilai True jika kedua ruas memenuhi syarat atau bernilai benar dan akan bernilai False jika salah satunya salah
+# contoh logika and 
+x=10
+print(x>7 and x<11)
+ 
+#True
+
+#logika or akan bernilai True jika salah satu ruas memenuhi syarat atay bernilai benar dan akan bernilai False jika dua-duanya benar
+# contoh logika or
+x=10
+print(x>7 or x<11)
+ 
+#True
+
+#logika not akan berkebalikan dari nilai yang ditentukan, jika syarat yang diberikan benar, maka akan bernilai False, dan sebaliknya akan bernilai True jika syarat yang diberikan salah
+# contoh logika not
+x=10
+print(not(x>7 or x<11))
+ 
+#False
