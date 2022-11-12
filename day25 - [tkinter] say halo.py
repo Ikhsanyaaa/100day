e787a85b495c795978c2cd0f15b41a5b23e148ac
ikhsan <ikhsanyaaa@gmail.com>
2022-11-12 21:03:28 +0800

    haloo

---
+++ day25 - [tkinter] say halo.py
@@ -0,0 +1,14 @@
+#membuat fungsi halo menggunakan library tkinter
+
+import tkinter
+from tkinter import *
+
+window = tkinter.Tk()
+window.title("ehem ehem")
+window.geometry("600x400")
+
+def say_hello():
+    tkinter.Label(window, text="haloo :)").pack()
+tkinter.Button(window, text= "klik disini!", command = say_hello).pack()
+
+window.mainloop()
