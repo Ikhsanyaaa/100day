15fe5b188a451c09cb7677e3c4e7f84e92312d95
ikhsan <ikhsanyaaa@gmail.com>
2022-11-17 20:03:07 +0800

    javaa

---
+++ day30 - first java/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String nama = "Ikhsan";
+        int umur = 19;
+        System.out.println("nama saya adalah " + nama);
+        System.out.println("umur saya adalah " + umur);
+    }
+}
