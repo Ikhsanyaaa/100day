91f2fad91745683cd98d8847d11555104f36416d
ikhsan <ikhsanyaaa@gmail.com>
2022-11-18 22:25:21 +0800

    operator pake tkinter

---
+++ day30 - first java/src/operator.java
@@ -0,0 +1,6 @@
+public class operator {
+    public static void main(String[] args) throws Exception {
+        int angka1 = 4;
+        int angka2 = 3;
+    }
+}
---
+++ day31 - kalkulator tkinter.py
@@ -0,0 +1,29 @@
+#membuat interface
+
+import tkinter as tk
+from tkinter import *
+
+window = tk.Tk()
+window.title("interface")
+window.geometry("300x200")
+
+def cetak():
+    angka1 = int(entry_angka1.get())
+    angka2= int(entry_angka2.get())
+    hasil = angka1 + angka2
+    label_nama = Label(window, text=hasil)
+    label_nama.pack()
+judul = Label(window, text="menampilkan hasil penjumlahan", font=15, bg = "blue", fg="white").pack() 
+label_angka1 = Label(window, text="angka 1 ")
+label_angka1.pack()
+entry_angka1 = Entry(window)
+entry_angka1.pack()
+nama = entry_angka1.get()
+label_angka2 = Label(window, text="angka 1 ")
+label_angka2.pack()
+entry_angka2 = Entry(window)
+entry_angka2.pack()
+nama = entry_angka2.get()
+tk.Button(window, text="cetak",command= cetak).pack()
+  
+window.mainloop()
