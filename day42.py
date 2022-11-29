0aa80d297d4968f44e9ad861551ca6df36db5868
ikhsan <ikhsanyaaa@gmail.com>
2022-11-29 21:14:08 +0800

    time set

---
+++ day42.py
@@ -0,0 +1,10 @@
+# program set timer
+import time
+
+detik = 1
+waktu = float(input("atur berapa menit yang anda inginkan"))
+while detik != (waktu*60) :
+    time.sleep(1)
+    print("detik : ", detik)
+    detik += 1
+print("selesai")
