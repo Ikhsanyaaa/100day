d38edf35e146a1e714eff612d7100212b173591d
ikhsan <ikhsanyaaa@gmail.com>
2022-11-30 19:01:13 +0800

    set timer tkinter

--- day43 - set timer tkinter.py
+++ day43 - set timer tkinter.py
@@ -1,15 +1,3 @@
-# program set timer
-# import time
-
-# detik = 1
-# waktu = float(input("atur berapa menit yang anda inginkan"))
-# while detik != (waktu*60) :
-#     time.sleep(1)
-#     print("detik : ", detik)
-#     detik += 1
-# print("selesai")
-
-
 import tkinter
 from tkinter import *
 import time
