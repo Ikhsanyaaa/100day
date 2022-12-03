459c2ea12cdfafc8c8e4a8b695543e48c8fe0217
ikhsan <ikhsanyaaa@gmail.com>
2022-12-03 18:40:34 +0800

    table mysql

---
+++ day46 - membuat table mysql dengan python.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+db = mysql.connector.connect(host = "localhost",user = "root",password = "",database = "")
+if db.is_connected():
+    print("berhasil terhubung dengan database")
+cursor = db.cursor()
+cursor.execute("create database datates")
+print("database berhasil dibuat!")
+cursor.execute("use datates")
+print("database berhasil digunakan!")
+cursor.execute("create table mhs(nim char(10) not null primary key, nama varchar(20) not null, umur int(2) not null)")
+print("table berhasil dibuat")
+nim = input("masukkan nim anda : ")
+nama = input("masukkan nama anda : ")
+umur = input("masukkan umur anda : ")
+cursor.execute(f'''insert into mhs 
+values 
+("{nim}","{nama}",{umur})''')
+db.commit()
+print("data berhasil ditambahkan ke dalam database")
